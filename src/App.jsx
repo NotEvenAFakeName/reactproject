b5bfc049ec05e55d76b87814bc3870c146ab93a8
NotEvenAFakeName <tobiasalbin10@hotmail.com>
2023-10-05 13:11:51 +0200

    save

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section className='container-fluid'>
+          <div>
+            <figure>
+              <img src="https://picsum.photos/200/300" alt="" />
+            </figure>
+
+
+            <p>
+            Vestibulum eu sapien eu ipsum imperdiet imperdiet. Nullam eu lobortis ipsum, id sollicitudin lorem. Integer nec dolor dolor. Nullam varius arcu orci, non congue felis pellentesque sit amet. In sed malesuada purus. Etiam vulputate mi cursus, varius augue nec, vehicula leo. Fusce id consequat orci. Suspendisse potenti. Sed quis arcu maximus, suscipit ipsum in, dignissim sapien. Donec vulputate turpis congue sapien aliquam, quis blandit nunc sagittis.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
+            </a>
+          </div>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
